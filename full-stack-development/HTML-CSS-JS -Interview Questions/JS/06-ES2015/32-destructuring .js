23e4942ca8bd5c7ec0449e2dbfc66f5b285e039b
Tanish <tanish.kumar.contact@gmail.com>
2026-02-03 13:26:05 +0530

    docs: move all code and questions HTML CSS JS Interview folder and split into Question and answer folder

--- full-stack-development/HTML-CSS-JS -Interview Questions/JS/06-ES2015/32-destructuring .js
+++
@@ -1,9 +0,0 @@
-// Before ES6, extracting values was verbose and repetitive:
-
-const user = { name: "Tanish", age: 21 };
-
-const name = user.name;
-const age = user.age;
-
-// ES6: Destructuring
-
--- full-stack-development/HTML-CSS-JS -Interview Questions/JS/14-js-working/02-js-working.js
+++
@@ -1,3 +0,0 @@
-console.log("A");
-console.log("B");
-console.log("C");
