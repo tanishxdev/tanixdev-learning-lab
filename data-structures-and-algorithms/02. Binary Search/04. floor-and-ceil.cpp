cbb6de701e81560b4cc5a95cf5b56c61456b6074
Tanish <tanish.kumar.contact@gmail.com>
2026-02-07 17:02:51 +0530

    feat: enhance floor and ceil functions with binary search and STL implementations

--- data-structures-and-algorithms/02. Binary Search/04. floor-and-ceil.cpp
+++ data-structures-and-algorithms/02. Binary Search/04. floor-and-ceil.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 // ⭐ Approach 1: Brute Force (Just for Understanding)
-
-pair<int, int> floorAndCeil(int arr[], int n, int x)
+pair<int, int> floorAndCeilBruteForce(int arr[], int n, int x)
 {
     int floorVal = -1;
     int ceilVal = -1;
+
+    // array should be sorted for correct floor & ceil
+    sort(arr, arr + n);
 
     // Find floor (largest <= x)
     for (int i = n - 1; i >= 0; i--)
@@ -30,20 +32,103 @@ pair<int, int> floorAndCeil(int arr[], int n, int x)
 
     return {floorVal, ceilVal};
 }
-// ⭐ Approach 2:
-// ⭐ Approach 3:
+
+// ⭐ Approach 2: Optimal (Using Binary Search)
+
+// Find FLOOR (largest <= x)
+int findFloor(vector<int> &arr, int x)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] <= x)
+        {
+            ans = arr[mid];
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+// Find CEIL (smallest >= x)
+int findCeil(vector<int> &arr, int x)
+{
+    int low = 0, high = arr.size() - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] >= x)
+        {
+            ans = arr[mid];
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+vector<int> getFloorAndCeil(int x, vector<int> &arr)
+{
+    sort(arr.begin(), arr.end());
+
+    int floorVal = findFloor(arr, x);
+    int ceilVal = findCeil(arr, x);
+
+    return {floorVal, ceilVal};
+}
+
+// ⭐ Approach 3: Optimal (Using STL)
+pair<int, int> floorAndCeilSTL(int arr[], int n, int x)
+{
+    vector<int> vec(arr, arr + n);
+    sort(vec.begin(), vec.end());
+
+    // Ceil using lower_bound
+    auto ceilIt = lower_bound(vec.begin(), vec.end(), x);
+    int ceilVal = (ceilIt != vec.end()) ? *ceilIt : -1;
+
+    // Floor using upper_bound
+    auto floorIt = upper_bound(vec.begin(), vec.end(), x);
+    int floorVal = (floorIt != vec.begin()) ? *(floorIt - 1) : -1;
+
+    return {floorVal, ceilVal};
+}
 
 int main()
 {
-    // test
-
     int arr[] = {1, 2, 8, 10, 10, 12, 19};
     int n = sizeof(arr) / sizeof(arr[0]);
     int x = 5;
 
-    auto ans = floorAndCeil(arr, n, x);
+    // Brute Force
+    pair<int, int> res1 = floorAndCeilBruteForce(arr, n, x);
+    cout << "Brute Force -> Floor: " << res1.first
+         << ", Ceil: " << res1.second << endl;
 
-    cout << "Floor: " << ans.first<< " " << "Ceil: " << ans.second << endl;
+    // Binary Search
+    vector<int> vec(arr, arr + n);
+    vector<int> res2 = getFloorAndCeil(x, vec);
+    cout << "Binary Search -> Floor: " << res2[0]
+         << ", Ceil: " << res2[1] << endl;
+
+    // STL
+    pair<int, int> res3 = floorAndCeilSTL(arr, n, x);
+    cout << "STL -> Floor: " << res3.first
+         << ", Ceil: " << res3.second << endl;
 
     return 0;
 }
